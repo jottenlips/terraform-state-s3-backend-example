f2242e0116f4a681ba58d15ccb94bbc7215d8022
John Ottenlips <johno@aloompa.com>
2022-08-03 14:18:17 -0500

    deploy s3 bucket

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Spin up neccessary resources for your terraform state
+
+resource "aws_s3_bucket" "terraform_states" {
+  bucket = "tf-states-s3backend"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "tf-lock-table"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket                  = "tf-states-s3backend"
+    key                     = "main.tf"
+    region                  = "us-east-1"
+    encrypt                 = true
+    profile                 = "default"
+    dynamodb_table          = "tf-lock-table"
+    shared_credentials_file = "$HOME/.aws/credentials"
+  }
+}
