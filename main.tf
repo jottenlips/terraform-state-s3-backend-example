38a116f3d9558a4f30372430038bfa3259bef74b
John Ottenlips <johno@aloompa.com>
2022-08-03 14:35:57 -0500

    enable bucket versioning

--- main.tf
+++ main.tf
@@ -4,17 +4,21 @@ resource "aws_kms_key" "tf_state_encryption_key" {
   deletion_window_in_days = 10
 }
 
-resource "aws_s3_bucket_versioning" "terraform_states" {
+resource "aws_s3_bucket" "terraform_states" {
   bucket = "tf-states-s3backend"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
+}
+
+# Enable versioning so we can see the full revision history of our
+# state files
+resource "aws_s3_bucket_versioning" "tf_versioning" {
+  bucket = aws_s3_bucket.terraform_states.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
-  bucket = aws_s3_bucket_versioning.terraform_states.bucket
+  bucket = aws_s3_bucket.terraform_states.bucket
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.tf_state_encryption_key.arn
